d3705004c3a0bf7d22660b86658ae9110e0adc5a
Quendora <quendora@gmail.com>
2017-07-24 13:48:39 +0200

    Commit all

--- genetic.js
+++ genetic.js
@@ -1,21 +1,19 @@
-var test={
-     a:undefined,
-     b:[2,2],
-     c:[3,3],
-};
-console.log(typeof 1);
-var check_genome= function (obj) {
-    if(obj.hasOwnProperty("a")===false || obj.hasOwnProperty("b")===false || obj.hasOwnProperty("c")===false)
-        return false;
+var test = {a:[6,7],b:[25,70],c:[9,34]};
+var picX = 60;
+var picY = 100;
 
 
+function validate(obj, picX, picY) {
 
+    if (typeof obj !== 'object') throw 'Error1';
 
-    if(typeof obj.a[0]!== "number" || typeof obj.a[1]!== "number" || typeof obj.b[0]!=="number"
-    || typeof obj.b[1]!== "number" || typeof obj.c[0]!== "number" || typeof obj.c[1]!=="number")
-        return false;
-    return true;
+    if (obj.hasOwnProperty('a') !== true || obj.hasOwnProperty('b') !== true || obj.hasOwnProperty('c') !== true) throw 'Error2';
+
+    if (typeof obj.a[0] !== 'number' || typeof obj.a[1] !== 'number' || typeof obj.b[0] !== 'number' || typeof obj.b[1] !== 'number' || typeof obj.c[0] !== 'number' || typeof obj.c[1] !== 'number') throw 'Error3';
+
+    if (obj.a[0] > picX || obj.b[0] > picX || obj.c[0] > picX || obj.a[1] > picY || obj.b[1] > picY || obj.c[1] > picY) throw 'Error4';
 
 }
 
-console.log(check_genome(test));
+validate(test, picX, picY);
+
