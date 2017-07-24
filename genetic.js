ab2135c190bf66c158ac8ed79625f7070c1a7781
Demongo <kuba.galat1@wp.pl>
2017-07-24 12:44:56 +0200

    hello

--- genetic.js
+++ genetic.js
@@ -1,3 +1,21 @@
-/**
- * Created by demongo on 22.07.17.
- */
+var test={
+     a:undefined,
+     b:[2,2],
+     c:[3,3],
+};
+console.log(typeof 1);
+var check_genome= function (obj) {
+    if(obj.hasOwnProperty("a")===false || obj.hasOwnProperty("b")===false || obj.hasOwnProperty("c")===false)
+        return false;
+
+
+
+
+    if(typeof obj.a[0]!== "number" || typeof obj.a[1]!== "number" || typeof obj.b[0]!=="number"
+    || typeof obj.b[1]!== "number" || typeof obj.c[0]!== "number" || typeof obj.c[1]!=="number")
+        return false;
+    return true;
+
+}
+
+console.log(check_genome(test));
