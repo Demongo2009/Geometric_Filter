eb572e42fd2246222368dfdbb43fbe8b7e9ed939
Quendora <quendora@gmail.com>
2017-08-16 12:46:26 +0200

    nothing

--- Generation.js
+++ Generation.js
@@ -3,7 +3,7 @@ function generation(population) {
     var best = population.slice(0, popLength * 0.2);
     var newOffspring = new Array();
 
-    for (var i = 0; i < popLength * 0.4; i++) {
+    for (var i = 0; i < popLength * 0.8; i++) {
 
         do {
 
@@ -18,21 +18,6 @@ function generation(population) {
     }
 
     var newGeneration = best.concat(Mutate(newOffspring));
-
-    for (var i = 0; i < popLength * 0.4; i++) {
-
-        var newGroupOfTriangles = new Object();
-
-        for (var j = 0; j < figuresLimit; j++) {
-
-            // if (Math.floor(Math.random() * 2) === 0)
-                newGroupOfTriangles[j] = new Triangle();
-            // else
-            // newGroupOfTriangles[j] = new Circle();
-        }
-
-        newGeneration.push(newGroupOfTriangles);
-    }
 
     generationNumber++;
     return newGeneration;
